e5237b697ebd85b9769d8f9ebdf48f81201f14c1
Vishal Maurya <mauryavishal9919@gmail.com>
2026-03-04 08:02:16 +0530

    fix resume pdf upload

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1039,7 +1039,9 @@ export const updateProfile = async (req, res) => {
         
         const file = req.file;
         const fileUri = getDataUri(file);
-        const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
+        const cloudResponse = await cloudinary.uploader.upload(fileUri.content, {
+           resource_type: "raw"
+            });
 
         let skillsArray;
         if (skills) {
